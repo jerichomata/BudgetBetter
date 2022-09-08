e2858163e87f0e97b1fc772591bb7491dd81a7f0
alex <alexzelinsky124@gmail.com>
2022-09-08 09:19:58 -0700

    demo user seeders updated

--- app/seeds/transactions.py
+++ app/seeds/transactions.py
@@ -4,10 +4,14 @@ from datetime import date
 # Adds transactions for the demo user
 def seed_transactions():
     transaction_1 = Transaction(title='Writing Salary', amount='9.99', date=date(2022, 7, 21), user_id=1)
-    transaction_2 = Transaction(title='Freelance Work', amount='99.99', date=date(2022, 7, 28), user_id=1)
+    transaction_2 = Transaction(title='Freelance Work', amount='100.00', date=date(2022, 7, 28), user_id=1)
+    transaction_3 = Transaction(title='Amazon Order', amount='-9.99', date=date(2022, 7, 28), user_id=1)
+    transaction_4 = Transaction(title='Scholarship Fund', amount='300.00', date=date(2022, 9, 1), user_id=1)
 
     db.session.add(transaction_1)
     db.session.add(transaction_2)
+    db.session.add(transaction_3)
+    db.session.add(transaction_4)
     db.session.commit()
 
 def undo_transactions():
